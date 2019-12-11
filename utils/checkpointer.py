e803a162548820b80940bfc26be68b67cdde0ddd
jeong-tae <make8286@naver.com>
2019-12-11 18:44:56 +0900

    fix: model save method and default value of comp rate

--- utils/checkpointer.py
+++ utils/checkpointer.py
@@ -3,23 +3,24 @@ import torch
 
 class Checkpointer(object):
     def __init__(self, model, optimizer=None, scheduler=None, 
-            save_dir="", checkpoint_period=10000, compressed_rate=100):
+            save_dir="", checkpoint_period=10000, comp_rate=0):
         self.model = model
         self.optimizer = optimizer
         self.scheduler = scheduler
         self.save_dir = save_dir
         self.checkpoint_period = checkpoint_period
-        self.compressed_rate = compressed_rate
+        self.comp_rate = comp_rate
         self.best_acc = 0
 
     def save(self, name):
         data = {}
-        data["model"] = self.model.state_dict()
-        data["compressed_rate"] = self.compressed_rate
+        data["model"] = self.model
+        data["state_dict"] = self.model.state_dict()
+        data["compression_rate"] = self.comp_rate
         if self.optimizer is not None:
-            data["optimizer"] = self.optimizer.state_dict()
+            data["optimizer"] = self.optimizer
         if self.scheduler is not None:
-            data["scheduler"] = self.scheduler.state_dict()
+            data["scheduler"] = self.scheduler
 
         save_file = os.path.join(self.save_dir, "{}.pth".format(name))
         torch.save(data, save_file)
