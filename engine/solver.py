cbb91559bf02d320b3173c05d6d44ba3db34f73e
jeong-tae <make8286@naver.com>
2019-12-11 18:09:20 +0900

    remove useless code

--- engine/solver.py
+++ engine/solver.py
@@ -1,21 +1,8 @@
 import torch
 
 def build_optimizer(args, model):
-    """
-    params = []
-    for key, value in model.named_parameters():
-        if not value.requires_grad:
-            continue
-        lr = args.lr
-        weight_decay = args.weight_decay
-        if "bias" in key:
-            lr = args.lr * 2 # BIAS_LR_FACTOR
-            weight_decay = 0 # no weight decay for bias
-        params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
-
+    # Not support other optimizer yet
     print(" [*] optimizer is SGD")
-    optimizer = torch.optim.SGD(params, args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
-    """
     optimizer = torch.optim.SGD(model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
     return optimizer
 
